ae00f186622c4122673086be09624db16f351552
Cindy T <ctang@Cindys-MacBook-Air.local>
2024-11-09 18:21:24 +0100

    project week 1

---
+++ report.md
@@ -0,0 +1,84 @@
+# Projet
+
+Raccourcis VScode : Cmd + Shift + P
+
+## Syntaxe
+
+Déclaration variables :
+
+`const count = ref<number>(0)`
+
+`const q1 = ref<string | null>(null)`
+
+`const filled = computed<boolean>(() => ...,)`
+
+Opérateurs logiques :
+
+`===`, `!==`, `&&`, `||`, `!`
+
+## Semaine 1
+
+### Vue.js
+
+Temps estimé : 10 minutes
+
+Temps passé : 10 minutes
+
+### Bootstrap
+
+Temps estimé : 15 minutes
+
+Temps passé : 15 minutes
+
+### Quiz
+
+Temps estimé : 1h30
+
+Temps passé : 2h
+
+Fichier : `QuizForm.Vue`
+
+#### Notes
+
+Quand on appuie sur Terminer, on passe à la prochaine question ?
+
+Un bouton après chaque question ?
+
+Score : compteur / nombre total de questions
+
+Comment faire un compteur avec `const`?
+
+function reset : juste remettre les values à null ?
+
+icons -> App.vue, `<i class="..."></i>`, https://icons.getbootstrap.com/
+
+#### Questions
+
+`main.ts`: importer les librairies externes (bootstrap) et internes (autres fichiers du repo)
+
+`main.css`: personaliser les styles (couleurs, ...)
+
+`App.vue`: html de la structure globale
+
+> `router/index.ts` : ??
+
+`AboutView.vue` : html de la page "about"
+
+`HomeView.vue` : html de la page "home"
+
+`QuizForm.vue` : le fonctionnement du quiz sur la page "home"
+
+> - Quelles sont les similarités et les différences entre `ref` et `computed` ?
+>   - `ref` est fixe et `computed`varie ?
+>   - Comment implémenter le compteur ?
+
+> - Que se passe-t-il lorsqu'on clique sur le bouton "Terminer" ?
+>   - Appel de la fonction `submit`
+
+- Qu'est-ce qu'un `v-model` ?
+
+  - `v-model` peut être utilisé sur un composant pour implémenter une liaison à double sens.
+  - https://fr.vuejs.org/guide/components/v-model
+
+- À quoi sert le `:class="{ disabled: !filled }"` ?
+  - On ne peut pas appuyer sur le bouton Terminer si pas toutes les questions sont remplies.
---
+++ src/components/QuizForm.vue
@@ -0,0 +1,204 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const q1 = ref<string | null>(null)
+const q2 = ref<string | null>(null)
+const q3 = ref<string | null>(null)
+const filled = computed<boolean>(
+  () => q1.value !== null && q2.value !== null && q3.value !== null,
+)
+
+function submit(event: Event): void {
+  event.preventDefault()
+  const count = ref<number>(0)
+
+  const n_questions = 3
+  if (q1.value === 'a11') {
+    count.value += 1
+  }
+  if (q2.value === 'a21') {
+    count.value += 1
+  }
+  if (q3.value === 'a33') {
+    count.value += 1
+  }
+  if (filled.value) {
+    alert(`Score : ${count.value}/${n_questions}`)
+    if (count.value === n_questions) {
+      alert(`Bravo !`)
+    }
+  }
+}
+
+function reset(): void {
+  q1.value = null
+  q2.value = null
+  q3.value = null
+}
+</script>
+
+<template>
+  <form @submit="submit">
+    <!-- Q1 -->
+    Le marcophage est ...
+    <div class="form-check">
+      <input
+        id="a11"
+        v-model="q1"
+        class="form-check-input"
+        type="radio"
+        name="q1"
+        value="a11"
+      />
+      <label class="form-check-label" for="a11"
+        >une cellule de l'immunité innée.</label
+      >
+    </div>
+    <div class="form-check">
+      <input
+        id="a12"
+        v-model="q1"
+        class="form-check-input"
+        type="radio"
+        name="q1"
+        value="a12"
+      />
+      <label class="form-check-label" for="a12"
+        >une cellule de l'immunité adaptative.</label
+      >
+    </div>
+    <div class="form-check">
+      <input
+        id="a13"
+        v-model="q1"
+        class="form-check-input"
+        type="radio"
+        name="q1"
+        value="a13"
+      />
+      <label class="form-check-label" for="a13">un événement.</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="a14"
+        v-model="q1"
+        class="form-check-input"
+        type="radio"
+        name="q1"
+        value="a14"
+      />
+      <label class="form-check-label" for="a14">un événement final.</label>
+    </div>
+
+    <!-- Q2 -->
+    Quelles cellules effectuent la phagocytose ?
+    <div class="form-check">
+      <input
+        id="a21"
+        v-model="q2"
+        class="form-check-input"
+        type="radio"
+        name="q2"
+        value="a21"
+      />
+      <label class="form-check-label" for="a21"
+        >Les cellules dendritiques.</label
+      >
+    </div>
+    <div class="form-check">
+      <input
+        id="a22"
+        v-model="q2"
+        class="form-check-input"
+        type="radio"
+        name="q2"
+        value="a22"
+      />
+      <label class="form-check-label" for="a22"
+        >Les lymphocytes T cytotoxiques.</label
+      >
+    </div>
+    <div class="form-check">
+      <input
+        id="a23"
+        v-model="q2"
+        class="form-check-input"
+        type="radio"
+        name="q2"
+        value="a23"
+      />
+      <label class="form-check-label" for="a23"
+        >Les lymphocytes T auxiliaires.</label
+      >
+    </div>
+    <div class="form-check">
+      <input
+        id="a24"
+        v-model="q2"
+        class="form-check-input"
+        type="radio"
+        name="q2"
+        value="a24"
+      />
+      <label class="form-check-label" for="a24">Les anticorps.</label>
+    </div>
+
+    <!-- Q3 -->
+    Le lymphocyte T cytotoxique...
+    <div class="form-check">
+      <input
+        id="a31"
+        v-model="q3"
+        class="form-check-input"
+        type="radio"
+        name="q3"
+        value="a31"
+      />
+      <label class="form-check-label" for="a31">produit des anticorps.</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="a32"
+        v-model="q3"
+        class="form-check-input"
+        type="radio"
+        name="q3"
+        value="a32"
+      />
+      <label class="form-check-label" for="a32">phagocyte la pathogène.</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="a33"
+        v-model="q3"
+        class="form-check-input"
+        type="radio"
+        name="q3"
+        value="a33"
+      />
+      <label class="form-check-label" for="a33"
+        >libère des perforines et granzymes.</label
+      >
+    </div>
+    <div class="form-check">
+      <input
+        id="a34"
+        v-model="q3"
+        class="form-check-input"
+        type="radio"
+        name="q3"
+        value="a34"
+      />
+      <label class="form-check-label" for="a34">libère des cytokines.</label>
+    </div>
+
+    <button
+      class="btn btn-primary"
+      :class="{ disabled: !filled }"
+      type="submit"
+    >
+      Terminer
+    </button>
+  </form>
+  <button class="btn btn-secondary" @click="reset">Réinitialiser</button>
+</template>
